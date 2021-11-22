df6c2843805d2156e46ad271dac6cdf3f5eef2d0
Ragini sharma <raginisharma6006845094@gmail.com>
2021-11-22 20:49:35 +0530

    swarg dance academy project

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const port = 8000;
+
+// EXPRESS SPECIFIC STUFF
+app.use('/static',express.static('static')) //'static' is the folder name
+app.use(express.urlencoded()) //help krta hai form ke data ko express tak laane ke liye ]
+
+//PUG SPECIFIC STUFF
+app.set('view engine', 'pug');//set the template engine as pug 
+app.set('views',path.join(__dirname,'views'))  //dirname=directory name
+
+//ENDPOINTS
+app.get('/',(req,res)=>{
+    const params = {}
+    res.status(200).render('index.pug',params);
+})
+
+
+
+
+
+//START THE SERVER
+app.listen(port,()=>{
+    console.log(`the application started successfully ${port}`);
+
+});
